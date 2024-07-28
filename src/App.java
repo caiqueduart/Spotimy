0f6a71ffce9ffd557380cbd2991fe440c6fca873
caiqueduart <dcaique242@gmail.com>
2024-07-28 19:56:01 -0300

    testanto a biblioteca swing

--- src/App.java
+++ src/App.java
@@ -1,34 +1,30 @@
+import java.awt.*;
+import java.net.http.WebSocket.Listener;
+
 import javax.swing.*;
-import classes.Teste;
-import classes.Valores;
 
-public class App {
+public class App extends JFrame{
 
+    JButton botao = new JButton("Clique em mim!");
+    JButton botao2 = new JButton("Clique em mim!");
+
+    App(){
+        setTitle("Spotimy");
+        setSize(1000, 700);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+        setLayout(null);
+        botao.setBounds(1, 1, botao.getPreferredSize().width, botao.getPreferredSize().height);
+        botao2.setBounds(botao.getWidth(), 1, botao.getPreferredSize().width, botao.getPreferredSize().height);
+        botao.setBackground(new Color(00,255,00));
+        add(botao);
+        add(botao2);
+        setVisible(true);
+    }
+    
     public static void main(String[] args) {
 
-        Object selectedValue1 = JOptionPane.showOptionDialog(
-            null, 
-            "Estou testando isso aqui, selecione uma opção", 
-            "Testanto isso", 
-            JOptionPane.YES_NO_OPTION, 
-            1, 
-            null, 
-            Valores.valores, 
-            null
-        );
-
-        JOptionPane.showMessageDialog(null, "O valor selecionado foi: " + Valores.valores[(int)selectedValue1]);
-
-        Object selectedValue2 = JOptionPane.showInputDialog(
-            null,
-            Teste.message,
-            Teste.title, 
-            JOptionPane.INFORMATION_MESSAGE,
-            null, 
-            Teste.opcao, 
-            Teste.opcao[0]
-        );
-
-        JOptionPane.showMessageDialog(null, "O valor selecionado foi: " + selectedValue2);
+        App app = new App();
     }
 }
---
+++ src/TesteComponents.java
@@ -0,0 +1,34 @@
+import javax.swing.*;
+import classes.Teste;
+import classes.Valores;
+
+public class TesteComponents {
+
+    public static void main(String[] args) {
+
+        Object selectedValue1 = JOptionPane.showOptionDialog(
+            null, 
+            "Estou testando isso aqui, selecione uma opção", 
+            "Testanto isso", 
+            JOptionPane.YES_NO_OPTION, 
+            1, 
+            null, 
+            Valores.valores, 
+            null
+        );
+
+        JOptionPane.showMessageDialog(null, "O valor selecionado foi: " + Valores.valores[(int)selectedValue1]);
+
+        Object selectedValue2 = JOptionPane.showInputDialog(
+            null,
+            Teste.message,
+            Teste.title, 
+            JOptionPane.INFORMATION_MESSAGE,
+            null, 
+            Teste.opcao, 
+            Teste.opcao[0]
+        );
+
+        JOptionPane.showMessageDialog(null, "O valor selecionado foi: " + selectedValue2);
+    }
+}
